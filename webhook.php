cbe2d46a396ee37f836c95445e96e627bfc33445
Tan Chun Siong <dreamtcs@hotmail.com>
2023-09-20 14:40:16 +0800

    added post / get for php webhook

--- webhook.php
+++ webhook.php
@@ -1,10 +1,13 @@
 <?php
 
 $config = include 'config.php';
-$secretToken= $config['webhook_app_secret_token'];
-// Get the raw POST data from the request
-$input = file_get_contents("php://input");
+$secretToken = $config['webhook_app_secret_token'];
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // This block handles POST requests
+
+    // Get the raw POST data from the request
+    $input = file_get_contents("php://input");
 
     // Decode the JSON data
     $data = json_decode($input);
@@ -16,7 +19,6 @@ $input = file_get_contents("php://input");
             // Get the plainToken from the payload
             $plainToken = $data->payload->plainToken;
 
-           
             // Hash the plainToken using HMAC-SHA256
             $encryptedToken = hash_hmac("sha256", $plainToken, $secretToken);
 
@@ -44,5 +46,21 @@ $input = file_get_contents("php://input");
         http_response_code(400); // Bad Request
         echo "Invalid event type.";
     }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // This block handles GET requests
 
+    // Handle your GET request logic here
+
+    // For example, you can retrieve query parameters using $_GET
+    if (isset($_GET['param1'])) {
+        $param1 = $_GET['param1'];
+        echo "Received GET request with param1 = $param1";
+    } else {
+        echo "Received GET request without param1";
+    }
+} else {
+    // Unsupported HTTP method
+    http_response_code(405); // Method Not Allowed
+    echo "Unsupported HTTP method.";
+}
 ?>
