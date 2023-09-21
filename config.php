1dce76f7c9c750e786e80a3c1e54c19cb5fea9b2
Tan Chun Siong <dreamtcs@hotmail.com>
2023-09-21 15:27:44 +0800

    updated with oauth redirect

--- config.php
+++ config.php
@@ -5,6 +5,8 @@ return [
     'client_secret' => 'yyyyyyy',
     'account_id' => 'zzzzzzz',
     'webhook_app_secret_token'=>'123123123',
+    'oauth_client_id'=>'xxxxxxxx',
+    'oauth_client_secret'=>'yyyyyy'
 ];
 
 ?>
